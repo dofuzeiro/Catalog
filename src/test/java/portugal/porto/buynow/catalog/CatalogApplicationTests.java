425d2d2b74d2d4403e4d26116d98bd3b2a6cc263
Rafael Ribeiro <rafa456@outlook.pt>
2022-09-27 22:37:06 +0100

    Base project createad from spring initializr

---
+++ src/test/java/portugal/porto/buynow/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package portugal.porto.buynow.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
